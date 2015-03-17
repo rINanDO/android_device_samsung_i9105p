55ec4338e4c9889e43a0d3123cb8070d9bb3c008
ghsr <ghsr92@yandex.ru>
2015-03-17 16:35:42 +0600

    i9105p: Initial SlimLP bringup

--- slim.mk
+++ slim.mk
@@ -1,11 +1,11 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/i9105p/full_i9105p.mk)
 
 PRODUCT_DEVICE := i9105p
-PRODUCT_NAME := cm_i9105p
+PRODUCT_NAME := slim_i9105p
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME=s2vepxx \
