554cf8788f4b057b651417d6401b760983f78642
ghsr <ghsr92@yandex.ru>
2015-07-22 16:38:24 +0600

    i9105p: Add TARGET_OTA_ASSERT_DEVICE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,3 +6,6 @@ TARGET_KERNEL_CONFIG := cyanogenmod_s2vep_defconfig
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/i9105p/ramdisk/fstab.capri_ss_s2vep
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := s2ve,s2vep,i9105,i9105p
