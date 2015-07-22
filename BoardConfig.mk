367b12f455a9c3b3bcf4423932458c8222c71387
ghsr <ghsr92@yandex.ru>
2015-07-22 16:38:24 +0600

    i9105p: Add TARGET_OTA_ASSERT_DEVICE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,9 @@ TARGET_RECOVERY_FSTAB := device/samsung/i9105p/ramdisk/fstab.capri_ss_s2vep
 # NFC
 BOARD_NFC_HAL_SUFFIX := capri
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := s2ve,s2vep,i9105,i9105p
+
 # SELinux
 BOARD_SEPOLICY_DIRS += \
     device/samsung/i9105p/sepolicy
