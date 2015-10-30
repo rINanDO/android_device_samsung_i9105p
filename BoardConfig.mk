fade0cdfc196ae829b9866c4d25806a339aaab6f
ghsr <ghsr92@yandex.ru>
2015-10-30 17:05:40 +0600

    i9105p: commonize ramdisk and kernel defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,6 @@
 # Include common BoardConfig
 -include device/samsung/galaxys2plus-common/BoardConfigCommon.mk
 
-# Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_s2vep_defconfig
-
-# Recovery
-TARGET_RECOVERY_FSTAB := device/samsung/i9105p/ramdisk/fstab.capri_ss_s2vep
-
 # NFC
 BOARD_NFC_HAL_SUFFIX := capri
 
--- cm.mk
+++ cm.mk
@@ -1,14 +1,19 @@
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
+# Inherit device configuration
 $(call inherit-product, device/samsung/i9105p/full_i9105p.mk)
 
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9105p
 PRODUCT_NAME := cm_i9105p
 
+# Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRODUCT_NAME=s2vepxx \
-	TARGET_DEVICE=s2vep \
-	BUILD_FINGERPRINT="samsung/s2vepxx/s2vep:4.2.2/JDQ39/I9105PXXUBNG1:user/release-keys" \
-	PRIVATE_BUILD_DESC="s2vepxx-user 4.2.2 JDQ39 I9105PXXUBNG1 release-keys"
+    PRODUCT_NAME=s2vepxx \
+    TARGET_DEVICE=s2vep \
+    BUILD_FINGERPRINT="samsung/s2vepxx/s2vep:4.2.2/JDQ39/I9105PXXUBNG1:user/release-keys" \
+    PRIVATE_BUILD_DESC="s2vepxx-user 4.2.2 JDQ39 I9105PXXUBNG1 release-keys"
