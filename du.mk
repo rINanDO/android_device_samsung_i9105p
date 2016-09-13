23126183a78117f37167d4baa065e5ffb855b490
Dominggoes Isakh <drjisakh@gmail.com>
2016-09-13 20:27:42 +0100

    i9105p: Build for Dirty Unicorns
    Change-Id: I701134e9854b21d4d5f6b0cd8fd0778c67d57158

--- du.mk
+++ du.mk
@@ -1,14 +1,19 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9105p/full_i9105p.mk)
 
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
+
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_i9105p
+PRODUCT_NAME := du_i9105p
 
 # Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
